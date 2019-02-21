0a70234dc75b465d7627cf401cc849517853b817
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-02-20 19:12:57 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require "pry"
+
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade].nil?
+      @roster[grade] = []
+    end
+      @roster[grade] << student_name
+    end
+
+    def grade(grade)
+      @roster[grade]
+    end
+
+    def sort
+      new_hash = Hash[@roster.map {|key,value| [key, value.sort]}]
+      @roster = new_hash
+    end
+
+end
